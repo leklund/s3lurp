3bc3d15c9926bb4b2e6347713731bd0fdb1b123b
Lukas Eklund <leklund@gmail.com>
2013-03-19 13:26:14 -0400

    move atr_accessor array into a contant i can re-use

--- lib/s3lurp.rb
+++ lib/s3lurp.rb
@@ -3,6 +3,15 @@ require 's3lurp/view_helpers'
 require 's3lurp/railtie' if defined? ::Rails::Railtie
 
 module S3lurp
+ VALID_CONFIG_KEYS = [
+      :s3_bucket, :s3_access_key, :s3_secret_key, :acl, :cache_control,
+      :content_type, :content_disposition, :content_encoding, :expires,
+      :success_action_redirect, :success_action_status,
+      :min_file_size, :max_file_size,
+      :amz_meta_tags, :minutes_valid,
+      :form_html_options, :file_field_tag_accept,
+      :submit_tag, :submit_tag_value, :submit_tag_options,
+      :file, :key].freeze
   class << self
     attr_accessor :config
   end
@@ -13,11 +22,8 @@ module S3lurp
     if config.file
       loaded_conf = YAML.load_file(Rails.root.join("config", config.file).to_s)
       env_conf = loaded_conf[Rails.env] || loaded_conf
-      %w( s3_bucket s3_access_key s3_secret_key ).each do |key|
-        if !env_conf.key?(key)
-          raise "Config file #{config.file} missing key #{key}"
-        end
-        config.send("#{key}=", env_conf[key])
+      VALID_CONFIG_KEYS.each do |key|
+        config.send("#{key.to_s}=", env_conf[key.to_s])
       end
     end
   end
@@ -27,14 +33,7 @@ module S3lurp
   end
 
   class Configuration
-    attr_accessor :s3_bucket, :s3_access_key, :s3_secret_key, :acl, :cache_control,
-      :content_type, :content_disposition, :content_encoding, :expires,
-      :success_action_redirect, :success_action_status,
-      :min_file_size, :max_file_size,
-      :amz_meta_tags, :minutes_valid,
-      :form_html_options, :file_field_tag_accept,
-      :submit_tag, :submit_tag_value, :submit_tag_options,
-      :file, :key
+    attr_accessor *VALID_CONFIG_KEYS
 
     def initialize
       @s3_bucket = ENV['S3_BUCKET'] || "S3_BUCKET"
